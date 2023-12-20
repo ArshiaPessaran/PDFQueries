32c212156082146ba16141b9559c0c28ad31c113
Arshia <Arshia286@gmail.com>
2023-12-20 00:34:38 -0500

    Sample Streamlit app created to envision responses

--- Chatbot/main.py
+++ Chatbot/main.py
@@ -1,27 +1,10 @@
-#import streamlit as st
-#st.title("Travel Buddy")
+import langchainhelper as lch
+import streamlit as st
 
-from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+st.title("Travel Buddy")
 
+submitted_location = st.sidebar.selectbox("Submit your location.", ("Orlando","New York","Houston"))
 
-from APIKey import OPENAI_API_KEY
-
-if OPENAI_API_KEY is None:
-    raise ValueError("OPEN_API_KEY is not set in the .env file")
-
-def generate_gas_prices(location):
-    llm = OpenAI(temperature=0.7, openai_api_key=OPENAI_API_KEY)
-    prompt_template_name = PromptTemplate(
-        input_variables = ['location'],
-        template = "I am in {location}, find me gas station prices."
-    )
-
-    prices_chain = LLMChain(llm=llm, prompt=prompt_template_name)
-    response = prices_chain({'location': location})
-    return response
-
-
-if __name__ == "__main__":
-    print(generate_gas_prices("orlando"))
+if submitted_location:
+    response = lch.generate_gas_prices(submitted_location)
+    st.text(response["GasPrices"])
