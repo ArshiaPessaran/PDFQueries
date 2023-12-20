b80f8adceb304663216f39f60d0f2e376aeab9b6
Arshia <Arshia286@gmail.com>
2023-12-19 23:27:27 -0500

    Update 2

--- Chatbot/main.py
+++ Chatbot/main.py
@@ -1,3 +1,27 @@
-import streamlit as st
+#import streamlit as st
+#st.title("Travel Buddy")
 
-st.title("Travel Buddy")
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
+
+from APIKey import OPENAI_API_KEY
+
+if OPENAI_API_KEY is None:
+    raise ValueError("OPEN_API_KEY is not set in the .env file")
+
+def generate_gas_prices(location):
+    llm = OpenAI(temperature=0.7, openai_api_key=OPENAI_API_KEY)
+    prompt_template_name = PromptTemplate(
+        input_variables = ['location'],
+        template = "I am in {location}, find me gas station prices."
+    )
+
+    prices_chain = LLMChain(llm=llm, prompt=prompt_template_name)
+    response = prices_chain({'location': location})
+    return response
+
+
+if __name__ == "__main__":
+    print(generate_gas_prices("orlando"))
